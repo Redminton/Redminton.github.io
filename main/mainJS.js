c82ced57041ab5bcaf4bd6e623ab12a262a4c7b7
Redminton <133177595+Redminton@users.noreply.github.com>
2023-09-19 05:34:26 -0300

    site v 2.1

--- main/mainJS.js
+++ main/mainJS.js
@@ -9,3 +9,21 @@ expandButton.addEventListener('click', () => {
 botaoRedes.addEventListener('click', () => {
     navigation2.classList.toggle('expanded2');
 });
+
+//Tema dark/light
+const changeColorButton = document.getElementById('tema');
+const body = document.body;
+
+// Define um array de cores que você deseja alternar
+const cores = ['#0f0f0f', /*'#565869',*/ '#ffffff'];
+
+let indiceCor = 1;
+// Adiciona um evento de clique ao botão
+changeColorButton.addEventListener('click', () => {
+    body.style.backgroundColor = cores[indiceCor];
+    indiceCor++;
+
+    if (indiceCor >= cores.length) {
+        indiceCor = 0;
+    }
+});
