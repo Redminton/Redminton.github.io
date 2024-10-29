cba873e1409d712f2ffeeee7d67a904e86c32382
João Vítor Dores dos Santos <133177595+Redminton@users.noreply.github.com>
2024-10-29 15:36:16 -0300

    fe

--- frameworks/script11.js
+++ frameworks/script11.js
@@ -1,8 +1,11 @@
+
 function calcular() {
     let dia = window.document.querySelector('input#inputdia');
     let resultado = window.document.querySelector('div#res');
     let semana = Number(dia.value);
+
     resultado.innerHTML = `<p>Você digitou o número: <strong>${semana}</strong><p>`;
+
 
     switch (semana) {
         case 1:
@@ -26,7 +29,36 @@ function calcular() {
         case 7:
             res.innerHTML += `<p>Sábado</p>`;
             break
-        default:
-            res.innerHTML += `<p>Erro Dia Inválido!</p>`;
+
     }
 }
+const diasemana = new Date().getDay();
+console.log('teste', diasemana);
+
+
+switch (diasemana) {
+    case 0:
+        res.innerHTML += `<p> hoje é Domingo</p>`;
+        break
+    case 1:
+        res.innerHTML += `<p> hoje é Segunda Feira</p>`;
+        break
+    case 2:
+        res.innerHTML += `<p> hoje éTerça-Feira</p>`;
+        break
+    case 3:
+        res.innerHTML += `<p> hoje é Quarta-Feira</p>`;
+        break
+    case 4:
+        res.innerHTML += `<p>hoje é Quinta-Feira</p>`;
+        break
+    case 5:
+        res.innerHTML += `<p> hoje é Sexta-Feira</p>`;
+        break
+    case 6:
+        res.innerHTML += `<p> hoje é Sábado</p>`;
+        break
+
+}
+
+
